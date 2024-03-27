ef0ff3bf94fc0e3b0b634cc1df7589da97b20098
Batuhan <batuhakisakol20@rcsi.ie>
2024-03-27 15:12:49 +0000

    Edit tests
    edit paths

--- code/rss_classification.py
+++ code/rss_classification.py
@@ -1,45 +1,49 @@
 import pandas as pd
-import numpy as np
-from xgboost import XGBClassifier
 from sklearn.preprocessing import StandardScaler
+from xgboost import XGBClassifier
 
-def RSSClassifier(df: pd.DataFrame, model_path: str = "../data/model/model_XgBoost_Rectal_Specific_Classifier.json",
-                 modules_path: str = "../data/gene_modules/filtered_genes_modules.txt", 
-                 scaled: bool = False, output: str = "RSS_predictions") -> None:
+
+def RSSClassifier(
+    df: pd.DataFrame,
+    model_path: str = "../data/model/model_XgBoost_Rectal_Specific_Classifier.json",
+    modules_path: str = "../data/gene_modules/filtered_genes_modules.txt",
+    scaled: bool = False,
+    output: str = "RSS_predictions",
+) -> None:
 
     """
-    Predicts the RSS clusters of the input data using the trained model and gene modules.  
-    Args:  
+    Predicts the RSS clusters of the input data using the trained model and gene modules.
+    Args:
         df: pd.DataFrame: The input data.  Rows should be samples and columns should be genes. First row should be the gene names (HUGO SYMBOLS). Data should be log2 transformed.
-        model_path: str: The path to the trained model.  
-        modules_path: str: The path to the modules file.  
-        scaled: bool: If the input data is scaled. Default is False.  
+        model_path: str: The path to the trained model.
+        modules_path: str: The path to the modules file.
+        scaled: bool: If the input data is scaled. Default is False.
         output: str: The output file name. Default is "RSS_predictions".
     Returns:
         None
     Writes:
-        A file with the predicted RSS clusters. 
+        A file with the predicted RSS clusters.
     """
-    
+
     clf = XGBClassifier()
     clf.load_model(model_path)
-    
-    genes_m = pd.read_csv(modules_path, sep="\t")
-    genes_m.columns = ["genes", "modules"]
-    groups_filtered_df = genes_m.set_index("genes")
-    
+
+    genes_modules = pd.read_csv(modules_path, sep="\t")
+    genes_modules.columns = ["genes", "modules"]
+    groups_filtered_df = genes_modules.set_index("genes")
+
     if scaled:
-        module_median2 = df.copy()
+        median_module = df.copy()
     else:
         scaler = StandardScaler()
-        module_median2 = scaler.fit_transform(df)
+        median_module = scaler.fit_transform(df)
 
-    module_median2 = pd.DataFrame(module_median2, index=df.index, columns=df.columns)
+    median_module = pd.DataFrame(median_module, index=df.index, columns=df.columns)
 
-    module_median2 = (
+    median_module = (
         pd.merge(
-            module_median2[
-                list(set(module_median2.columns) & set(groups_filtered_df.index))
+            median_module[
+                list(set(median_module.columns) & set(groups_filtered_df.index))
             ].T,
             groups_filtered_df,
             left_index=True,
@@ -50,14 +54,14 @@ def RSSClassifier(df: pd.DataFrame, model_path: str = "../data/model/model_XgBoo
         .T
     )
 
-    module_median2 = module_median2.drop("0", axis=1)
+    median_module = median_module.drop("0", axis=1)
 
-    predicted_labels = clf.predict(module_median2)
+    predicted_labels = clf.predict(median_module)
 
     clusters2 = pd.DataFrame(
-        predicted_labels, index=module_median2.index, columns=["RSS"]
+        predicted_labels, index=median_module.index, columns=["RSS"]
     )
-    
+
     clusters2.RSS.replace({"C1": "RSS1", "C2": "RSS2", "C3": "RSS3"}, inplace=True)
 
     clusters2.to_csv("{out}.txt".format(out=output), sep="\t")
