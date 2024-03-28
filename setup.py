27abd1327cb3d8c8128447652373c69f8d9496d9
Batuhan <batuhakisakol20@rcsi.ie>
2024-03-28 10:34:18 +0000

    Change function name

--- setup.py
+++ setup.py
@@ -3,7 +3,13 @@ from setuptools import setup, find_packages
 setup(
     name='rssclassifier',
     version='0.1',
+    author='Batuhan Kisakol',
+    author_email='batuhankisakol@gmail.com',
+    url='https://github.com/kisakol/RSS_Classifier',
     packages=find_packages(),
+    package_data={
+        'my_package': ['data/gene_modules/*.txt', 'data/model/*'],
+    },
     install_requires=[
         'numpy==1.21.0',
         'pandas==1.2.1',
